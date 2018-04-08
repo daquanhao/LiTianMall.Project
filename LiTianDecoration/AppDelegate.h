fba37ccf65925f9dfe7c2dd921d991387349f7c3
administrator <administrator@administratordeimac.local>
2018-04-08 10:46:49 +0800

    Initial Commit

---
+++ LiTianDecoration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiTianDecoration
+//
+//  Created by administrator on 2018/4/8.
+//  Copyright © 2018年 administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
