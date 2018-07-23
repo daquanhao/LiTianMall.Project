378d708b216a27e1a6ba33a2f644356ee6dc3c7a
administrator <administrator@administratordeimac.local>
2018-07-23 15:38:38 +0800

    第四次修改商品详情

---
+++ LiTianDecoration/Class/Mall(商城模块)/MallClass(商城分类)/Controller/HSQSearchBarViewController.h
@@ -0,0 +1,18 @@
+//
+//  HSQSearchBarViewController.h
+//  LiTianDecoration
+//
+//  Created by administrator on 2018/5/3.
+//  Copyright © 2018年 administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HSQSearchBarViewController : UIViewController
+
+/**
+ * @brief 搜索的提示文字
+ */
+@property (nonatomic, copy) NSString *SearchPlacherString;
+
+@end
